16441b8a382c51cb820bafcb92a781eba9181cb5
Don <don.j.olmstead@gmail.com>
2016-03-12 18:29:46 -0800

    Create the analyzer library

---
+++ lib/analyzer.dart
@@ -0,0 +1,10 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/analyzer/reflectable_annotation.dart';

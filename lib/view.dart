8cf632c0d8ec538370059330d658b42cbab2dcc4
Don <don.j.olmstead@gmail.com>
2016-03-12 18:29:00 -0800

    Create view library

---
+++ lib/view.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2016, the Dogma Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'src/view/model_proxy_library_view.dart';
+export 'src/view/model_proxy_view.dart';
